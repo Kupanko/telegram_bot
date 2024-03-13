a6ce6986fdde6af8e2bbe2f92855c9d9aefedc84
Kupano <24877137+Kupanko@users.noreply.github.com>
2024-03-13 13:33:25 +0300

    Create launch.js

---
+++ launch.js
@@ -0,0 +1,19 @@
+const TeleClass = require('./body');
+const bot = new TeleClass({ token: "your_bot_token" });
+
+bot.start();
+
+bot.on('text', async (text, chat, raw) => {
+    bot.send('message', { chat_id: chat.id, text: 'message' });
+    // bot.send('photo', { chat_id: chat.id, photo: 'url to image', caption: 'caption' });
+
+    bot.setReaction({ chat_id: chat.id, message_id: raw.message_id, reaction: [{ type: 'emoji', emoji: '❤' }] });
+});
+
+bot.on('photo', async (photos, chat, raw) => {
+    const fileId = photos[photos.length - 1].file_id;
+
+    bot.getFileLink(fileId).then(link => {
+        bot.send('message', { chat_id: chat.id, text: `Got your image! Link: <${link}>` });
+    })
+});
